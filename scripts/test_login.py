5851a8711e60fc664c38f086d1ddf2508de8ea33
cxt20200303 <15557110918@163.com>
2020-03-05 11:43:52 +0800

    first commit

---
+++ scripts/test_login.py
@@ -0,0 +1,19 @@
+import allure
+
+import pytest
+class TestLogin:
+
+    def test_login1(self):
+        assert 1
+
+    def test_login2(self):
+        assert 0
+
+
+    def test_login3(self):
+        assert 0
+
+
+    def test_login4(self):
+        assert 1
+
